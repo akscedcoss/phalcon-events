3937a891e5e2a855f58fba0bb0588eee7b1096b5
cedcoss <amnakumarsrivastava@cedcoss.com>
2022-03-29 14:40:07 +0530

    inital Structure project

---
+++ src/app/controllers/ProductsController.php
@@ -0,0 +1,20 @@
+<?php
+
+use Phalcon\Mvc\Controller;
+
+
+class ProductsController extends Controller
+{
+    public function indexAction()
+    {
+      
+        
+        echo "i am in products index";
+    }
+    public function addAction()
+    {
+        echo "i am here to add new products";
+    }
+
+
+}
---
+++ src/app/models/Products.php
@@ -0,0 +1,12 @@
+<?php
+
+use Phalcon\Mvc\Collection;
+
+Class Products extends Collection{
+
+
+    public $id;
+    public $name;
+
+    
+}
